bab6c0f0690edc5f361f1ab71afb5427fdbe6811
AnimeshDasLtts <91518211+AnimeshDasLtts@users.noreply.github.com>
2024-01-12 17:55:38 +0530

    Update calculator_test.cpp

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -1,1 +1,28 @@
 
+// calculator_test.cpp
+#include "CppUTest/TestHarness.h"
+#include "calculator.cpp"
+
+TEST_GROUP(Calculator) {
+    Calculator calc;
+};
+
+TEST(Calculator, Addition) {
+    CHECK_EQUAL(8, calc.add(5, 3));
+}
+
+TEST(Calculator, Subtraction) {
+    CHECK_EQUAL(2, calc.subtract(5, 3));
+}
+
+TEST(Calculator, Multiplication) {
+    CHECK_EQUAL(15, calc.multiply(5, 3));
+}
+
+TEST(Calculator, Division) {
+    CHECK_EQUAL(2, calc.divide(6, 3));
+}
+
+TEST(Calculator, DivisionByZero) {
+    CHECK_EQUAL(0, calc.divide(6, 0));
+}
