2be0e591bc8a4a5ff1c557664756f0bc3f5a5d34
AnimeshDasLtts <91518211+AnimeshDasLtts@users.noreply.github.com>
2024-01-17 15:08:22 +0530

    Update TestHarness.cpp

--- test/TestHarness.cpp
+++ test/TestHarness.cpp
@@ -1,1 +1,7 @@
 
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int argc, char** argv) {
+    // Run the CppUTest test runner with command-line arguments
+    return CommandLineTestRunner::RunAllTests(argc, argv);
+}
